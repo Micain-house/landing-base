6524b444f5c95b1708109ed45818a1bbc80f7ded
Micain-house <mmp@in-house.company>
2024-05-27 16:59:22 -0400

    home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,105 @@
 import React from 'react';
 import './Home.css'
-import SearchBar from '../../components/SearchBar/SearchBar';
-import Princing from '../Pricing/Pricing';
+import Button from 'react-bootstrap/Button';
+import cardHome from '../../assets/img-blog2.webp'
+import Accordion from 'react-bootstrap/Accordion';
+
 
 const Home = () => {
   return (
+    <>
     <div className='home'>
+      <div className='home-container'>
      <h1>HOME</h1>
-     <SearchBar/>
+     <h2>LOREM IMPSUM</h2>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+       <div className='buttons-home'>
+       <Button variant="primary" className="btn-home-contact">CONTACT</Button>
+       <Button variant="primary" className="btn-home-products">PRODUCTS</Button>
+       </div>
+       <section>
+        <div className='card-home'>
+         <img src={cardHome} alt='cardHome'/>
+         <div className='card-home-text'>
+         <h3>LOREM IMPSUM</h3>
+         <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium"</p>
+         <Button variant="primary" className="btn-home-products">Primary</Button>
+         </div>
+        </div>
+       </section>
+       <section>
+        <div className='card-home'>
+          <div className='card-home-text'>
+         <h3>LOREM IMPSUM</h3>
+         <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium"</p>
+         <Button variant="primary" className="btn-home-products">Primary</Button>
+         </div>
+         <img src={cardHome} alt='cardHome'/>
+        </div>
+       </section>
+
+       <section>
+        <div className='preguntas-frecuentes'>
+          <h4>PREGUNTAS FRECUENTES</h4>
+        <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+
+        </div>
+       </section>
+     </div>
+    
     </div>
+
+    </>
   );
 }
 
